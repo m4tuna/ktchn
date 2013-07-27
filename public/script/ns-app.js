859be42099edd84c2095d5a66f7d5e8df9f92131
Shawn Marincas <smarincas@ephare.com>
2013-07-27 09:00:19 -0400

    facebook and twitter login

--- public/script/ns-app.js
+++ public/script/ns-app.js
@@ -12,25 +12,53 @@ angular
 		$routeProvider
 			.when('/', { templateUrl: '/views/home.html', controller: 'HomeCtrl' })
 			.when('/about', { templateUrl: '/views/about.html', controller: 'HomeCtrl' })
-			.when('/login', { templateUrl: '/views/login.html', controller: 'HomeCtrl' })
+			.when('/login', { templateUrl: '/views/login.html', controller: 'LoginCtrl' })
 			.when('/board', { templateUrl: '/views/board.html', controller: 'BoardCtrl' })
 			.when('/recipes', { templateUrl: '/views/recipes.html', controller: 'HomeCtrl' })
 			.otherwise('/404', { templateUrl: '/views/404.html', controller: 'DefaultCtrl' });
 	}
-]).controller('TopCtrl',[ '$scope', '$rootScope', 'AppState',
-	function($scope, $rootScope, AppState) {
+]).controller('TopCtrl',[ '$scope', '$location', 'FireAuth', 'AppState',
+	function($scope, $location, FireAuth, AppState) {
 		console.log('initialize Top Control');
 		$scope.state = AppState;
+
+		$scope.logout = function() {
+			FireAuth.logout();
+
+			delete $scope.state.user;
+			$location.path('/');
+		}
+
 	}
-]).controller('DefaultCtrl', [ '$scope', 'AppState',
+]).controller('DefaultCtrl', [ '$scope', 'AppState',      //read_friendlists
 	function($scope, AppState) {
 		console.log('initialize Default Control');
 		$scope.state = AppState;
 	}
-]).controller('BoardCtrl', [ '$scope', 'AppState',
-	function($scope, AppState) {
+]).controller('LoginCtrl', [ '$scope', '$location', 'AppState', 'FireAuth',
+	function($scope, $location, AppState, FireAuth) {
+		$scope.state = AppState;
+
+		$scope.login = function(provider) {
+			FireAuth.login(provider);
+		}
+
+		$scope.$on('FireBase::Login', function(user) {
+			console.log('all logged in and ready to go!');
+
+			$scope.$apply(function() {
+				$location.path('/board');
+			});
+		})
+	}
+]).controller('BoardCtrl', [ '$scope', '$location', 'AppState', 'FireAuth',
+	function($scope, $location, AppState, FireAuth) {
 		console.log('initialize Board Control');
 		$scope.state = AppState;
+
+		if(!$scope.state.user) {
+			$location.path('/login');
+		}
 
 		$scope.state.boards = [
 			{
--- public/script/ns-services.js
+++ public/script/ns-services.js
@@ -9,4 +9,23 @@ angular
 .module('nsServices', [])
 .service('AppState', function() {
 	return { };
+}).service('FireAuth', function(AppState,$rootScope) {
+	var fireRef = new Firebase('https://ktchn.firebaseio.com/');
+
+	return new FirebaseSimpleLogin(fireRef, function(err, user) {
+		if(err) {
+			console.log(err);
+			return;
+		}
+
+		if(!user) {
+			console.log('user is logged out');
+			return;
+		}
+
+		console.log('logging in!', user);
+		AppState.user = user;
+
+		$rootScope.$broadcast('FireBase::Login',user);
+	});
 });
